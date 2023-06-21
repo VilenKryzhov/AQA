3340bbae2487ada0523cdb3ae01f67c7ca9bcb50
Vilen <krijov.vilen@gmail.com>
2023-06-21 16:57:47 +0300

    Update commit

---
+++ src/test/java/TestLogin/TestLogin.java
@@ -0,0 +1,14 @@
+package TestLogin;
+
+import org.base.BaseTest;
+import org.pages.GeneretorMethods;
+import org.testng.annotations.Test;
+
+public class TestLogin extends BaseTest {
+    @Test
+    public void test(){
+        GeneretorMethods.logInTest().typeLoginInput("standard_user");
+        GeneretorMethods.logInTest().typePasswordInput("secret_sauce");
+        GeneretorMethods.logInTest().clickEnterButton();
+    }
+}
