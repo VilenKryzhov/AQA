bc2d597554c0d0ac5776b150cc25d247268d50e5
Vilen <krijov.vilen@gmail.com>
2023-06-28 11:20:53 +0300

    Update commit

--- src/test/java/SauceWebsiteTest/TestLogin/TestLogin.java
+++ src/test/java/SauceWebsiteTest/TestLogin/TestLogin.java
@@ -1,11 +1,12 @@
 package SauceWebsiteTest.TestLogin;
 
 import org.base.BaseTest;
+import org.junit.runner.Description;
 import org.saucePages.GeneretorMethods;
 import org.testng.annotations.Test;
 
 public class TestLogin extends BaseTest {
-    @Test
+    @Test(description = "[AQA-2]: Login Test")
     public void test(){
         GeneretorMethods.logInTest().typeLoginInput("standard_user");
         GeneretorMethods.logInTest().typePasswordInput("secret_sauce");
