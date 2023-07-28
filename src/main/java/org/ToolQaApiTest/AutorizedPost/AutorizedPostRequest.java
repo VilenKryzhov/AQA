fa1caec3247077854f2cd2ec48649f77344e4232
VilenKryzhov <krijov.vilen@gmail.com>
2023-07-28 16:23:04 +0300

    Update commit

---
+++ src/main/java/org/ToolQaApiTest/AutorizedPost/AutorizedPostRequest.java
@@ -0,0 +1,11 @@
+package org.ToolQaApiTest.AutorizedPost;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AutorizedPostRequest {
+    private String userName;
+    private String password;
+}
